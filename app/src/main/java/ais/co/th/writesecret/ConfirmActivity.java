23f38259259f6a044804ffba0268d118ecb47d5b
Jirachai Hongthong <jirachai_h1@hotmail.com>
2015-03-18 14:59:25 +0700

    update confrim

--- app/src/main/java/ais/co/th/writesecret/ConfirmActivity.java
+++ app/src/main/java/ais/co/th/writesecret/ConfirmActivity.java
@@ -2,22 +2,47 @@ package ais.co.th.writesecret;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class ConfirmActivity extends ActionBarActivity {
     SharedPreferences prefs;
     String secret;
+    Button btnOK;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+        btnOK = (Button) findViewById(R.id.btn_ok);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        secret = prefs.getString("Secret", "");
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnSecret();
+            }
+        });
+
+        if(secret.matches("")) {
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivityForResult(intent, 100);
+        }
+
+
+
 
     }
     private void returnSecret() {
+        secret = prefs.getString("Secret", "");
         if (!secret.matches("")) {
             Intent data = new Intent();
             data.putExtra("Secret", secret);
@@ -26,6 +51,13 @@ public class ConfirmActivity extends ActionBarActivity {
         }
     }
 
-
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (100 == requestCode) {
+            if (resultCode == RESULT_OK) {
+                returnSecret();
+            }
+        }
+    }
 }
--- app/src/main/java/ais/co/th/writesecret/MainActivity.java
+++ app/src/main/java/ais/co/th/writesecret/MainActivity.java
@@ -1,13 +1,10 @@
 package ais.co.th.writesecret;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,35 +13,55 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
     EditText inputSC;
-    Button btnSave;
+    Button btnLogin;
     SharedPreferences prefs;
     String secret;
+    Button btnLogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inputSC = (EditText) findViewById(R.id.input_sc);
-        btnSave = (Button) findViewById(R.id.btn_save);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogOut = (Button) findViewById(R.id.btn_logout);
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        btnSave.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 secret = inputSC.getEditableText().toString();
                 if (!secret.matches("")) {
                     prefs.edit().putString("Secret", secret).apply();
-                    Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_LONG).show();
+
                     returnSecret();
                 }
             }
         });
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prefs.edit().putString("Secret", "").apply();
+
+                secret = "";
+                inputSC.setText("");
+                Toast.makeText(getApplicationContext(), "LogOut Success", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
         secret = prefs.getString("Secret", "");
+
         if (!secret.matches("")) {
-            returnSecret();
+
+            inputSC.setText(secret);
+
+        }else{
+
         }
     }
 
     private void returnSecret() {
+        secret = prefs.getString("Secret", "");
         if (!secret.matches("")) {
             Intent data = new Intent();
             data.putExtra("Secret", secret);
